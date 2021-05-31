ad48cefc287dee76661581008b66992d8f55cf9e
Brus <201615602e@utea.edu.pe>
2021-05-31 10:00:38 -0500

    Modificado Practica de Botones

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
+//
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(
-    MyApp(),
-  );
-}
+void main() => runApp(Boton1());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var city = ['Toronto', 'Boston', 'Mexico', 'London'];
-  var firstcity = 'Toronto';
+class Boton1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,53 +12,54 @@ class _MyAppState extends State<MyApp> {
           title: Text('Buttons'),
         ),
         body: SafeArea(
-          child: Column(children: <Widget>[
-            Center(
-              child: SafeArea(
-                child: Text(
-                  "Botones",
-                  style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black,
-                      fontSize: 25.0),
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: SafeArea(
+                  child: Text(
+                    "Botones",
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                        fontSize: 25.0),
+                  ),
                 ),
               ),
-            ),
-            // Raised Button
-            RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                'Raised Button',
-                style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.white,
-                    fontSize: 20.0),
+              // Raised Button
+              RaisedButton(
+                color: Colors.blue,
+                child: Text(
+                  'Raised Button',
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                      fontSize: 20.0),
+                ),
+                onPressed: () {},
               ),
-              onPressed: () {},
-            ),
-            // Icon Button
-            IconButton(
-              icon: Icon(Icons.volume_up),
-              tooltip: 'Increase volume by 10',
-              onPressed: () {},
-            ),
-            Text('Sound'),
-            // Flat Button
-            FlatButton(
-              color: Colors.blue,
-              textColor: Colors.white,
-              disabledColor: Colors.grey,
-              disabledTextColor: Colors.black,
-              padding: EdgeInsets.all(8.0),
-              splashColor: Colors.blueAccent,
-              onPressed: () {},
-              child: Text(
-                "Flat Button",
-                style: TextStyle(fontSize: 20.0),
+              // Icon Button
+              IconButton(
+                icon: Icon(Icons.volume_up),
+                tooltip: 'Increase volume by 10',
+                onPressed: () {},
               ),
-            ),
-// End of DropdownButton Code //
-          ]),
+              Text('Sound'),
+              // Flat Button
+              FlatButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                disabledColor: Colors.grey,
+                disabledTextColor: Colors.black,
+                padding: EdgeInsets.all(8.0),
+                splashColor: Colors.blueAccent,
+                onPressed: () {},
+                child: Text(
+                  "Flat Button",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
